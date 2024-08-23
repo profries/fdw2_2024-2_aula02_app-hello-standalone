523070bde54045bf79a2330e6c16ebbfecac9234
Luis Henrique Ries <prof.ries@gmail.com>
2024-08-23 19:12:32 -0300

    Modificando template e adicionando os componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    FormsModule,
+    MeuComponenteComponent,
+    MyComponentComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-hello-standalone';
+  title = 'World';
+  name = 'everybody';
+  dispararAlerta() {
+    alert("Disparado o alerta");
+    this.name= 'Ries'    
+  }
 }
